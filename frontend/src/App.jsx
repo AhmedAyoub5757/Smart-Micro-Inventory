5a4b743bd9f794bd718f1e8b00ee9e5534791ba5
AhmedAyoub5757 <kkahmed5757@gmail.com>
2025-09-19 14:56:11 +0500

    basic structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+      <div className="h-screen flex items-center justify-center bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">
+          🛒 Smart Micro-Inventory
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
